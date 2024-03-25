f2edb457abf51d7e2af32bf651a39fc99ac933d6
rayeraye1014 <rayeraye1014@gmail.com>
2024-03-26 01:42:45 +0800

    update carousel

--- components/common/carousel.js
+++ components/common/carousel.js
@@ -10,11 +10,18 @@ import Carousel from 'react-bootstrap/Carousel'
 
 export default function CarouselS1({ pageName = '' }) {
   const [isHovered, setIsHovered] = useState(false)
-  const handleMouseEnter = () => {
-    setIsHovered(true)
+  const [isHoveredSmallOne, setIsHoveredSmallOne] = useState(false)
+  const [isHoveredSmallTwo, setIsHoveredSmallTwo] = useState(false)
+
+  const handleMouseEnter = (type) => {
+    if (type === 'big') setIsHovered(true)
+    if (type === 'smallOne') setIsHoveredSmallOne(true)
+    if (type === 'smallTwo') setIsHoveredSmallTwo(true)
   }
-  const handleMouseLeave = () => {
-    setIsHovered(false)
+  const handleMouseLeave = (type) => {
+    if (type === 'big') setIsHovered(false)
+    if (type === 'smallOne') setIsHoveredSmallOne(false)
+    if (type === 'smallTwo') setIsHoveredSmallTwo(false)
   }
 
   return (
@@ -30,135 +37,183 @@ export default function CarouselS1({ pageName = '' }) {
               <Carousel.Item
                 style={{
                   background: isHovered ? '#48494980' : '',
+                  borderRadius: isHovered ? '5px' : '5px',
                 }}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
+                onMouseEnter={() => handleMouseEnter('big')}
+                onMouseLeave={() => handleMouseLeave('big')}
               >
-                <div style={{ borderRadius: '5px', overflow: 'hidden' }}>
+                <div className={style.adArea}>
                   <Image
                     text="First slide"
                     src="/ad1.png"
                     alt=""
                     width={1500}
                     height={680}
-                    style={{
-                      opacity: '0.9',
-                      borderRadius: '5px',
-                    }}
+                    className={style.adImg}
                   />
                 </div>
                 <Carousel.Caption
                   style={{
-                    marginBottom: '230px',
+                    marginBottom: '130px',
                   }}
                 >
-                  <h1 style={{ fontSize: '80px' }}>
+                  <h1 className={style.title}>
                     <strong>Let's make a DEAL</strong>
                   </h1>
-                  <p>
+                  <p className={style.txt}>
                     <strong>挖掘寶物，也可以愛護地球</strong>
                   </p>
-                  <button type="button" className="btn btn-outline-secondary">
-                    Secondary
-                  </button>
+                  <Link href="">
+                    <button type="button" className="btn btn-outline-dark">
+                      了解更多
+                    </button>
+                  </Link>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item
                 style={{
                   background: isHovered ? '#48494980' : '',
+                  borderRadius: isHovered ? '5px' : '5px',
                 }}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
+                onMouseEnter={() => handleMouseEnter('big')}
+                onMouseLeave={() => handleMouseLeave('big')}
               >
-                <div style={{ borderRadius: '5px', overflow: 'hidden' }}>
+                <div className={style.adArea}>
                   <Image
                     text="Second slide"
                     src="/woman2.jpg"
                     alt=""
                     width={1500}
                     height={680}
-                    style={{
-                      opacity: '0.9',
-                    }}
+                    className={style.adImg}
                   />
                 </div>
-                <Carousel.Caption style={{ marginBottom: '230px' }}>
-                  <h1>WOMAN'S DAY</h1>
-                  <p>
+                <Carousel.Caption
+                  style={{
+                    marginBottom: '150px',
+                  }}
+                >
+                  <h1 className={style.title2}>WOMAN'S DAY</h1>
+                  <p className={style.txt2}>
                     <strong>女裝折扣 蓄勢待發</strong>
                   </p>
-                  <button type="button" className="btn btn-outline-secondary">
-                    Secondary
-                  </button>
+                  <Link href="">
+                    <button type="button" className="btn btn-outline-dark">
+                      了解更多
+                    </button>
+                  </Link>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item
                 style={{
                   background: isHovered ? '#3b3c3c80' : '',
-                  borderRadius: '5px',
+                  borderRadius: isHovered ? '5px' : '5px',
                 }}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
+                onMouseEnter={() => handleMouseEnter('big')}
+                onMouseLeave={() => handleMouseLeave('big')}
               >
-                <div style={{ borderRadius: '5px', overflow: 'hidden' }}>
+                <div className={style.adArea}>
                   <Image
                     text="Third slide"
                     src="/globalday.jpg"
                     alt=""
                     width={1500}
                     height={680}
-                    style={{
-                      opacity: '0.9',
-                      borderRadius: '5px',
-                    }}
+                    className={style.adImg}
                   />
                 </div>
-                <Carousel.Caption style={{ marginBottom: '230px' }}>
-                  <h1>世界地球日</h1>
-                  <p>
+                <Carousel.Caption
+                  style={{ marginBottom: '100px', marginLeft: '470px' }}
+                >
+                  <h1 className={style.title3}>世界地球日</h1>
+                  <p className={style.txt3}>
                     <strong>小碳點大翻倍</strong>
                   </p>
-                  <button type="button" className="btn btn-outline-secondary">
-                    Secondary
-                  </button>
+                  <Link href="">
+                    <button type="button" className="btn btn-outline-dark">
+                      了解更多
+                    </button>
+                  </Link>
                 </Carousel.Caption>
               </Carousel.Item>
             </Carousel>
           </div>
           <div className="col-lg-4" style={{ paddingLeft: '30px' }}>
-            <div className={`product-offer mb-2 rounded ${style.news}`}>
+            <div className={`mb-2 rounded ${style.bg1}`}>
+              <Image
+                src="/father.png"
+                alt=""
+                width={600}
+                height={335}
+                className={isHoveredSmallOne ? style.hover : style.nohover}
+                onMouseEnter={() => handleMouseEnter('smallOne')}
+                onMouseLeave={() => handleMouseLeave('smallOne')}
+              />
               <div className={style.content}>
-                <h3 className="mb-3">爸爸辛苦了！</h3>
-                <h6 className="text-uppercase">
+                <h1 className={`mt-3 ${style.shadow}`}>
                   <strong>\ 父親節大折扣 /</strong>
-                </h6>
-                <Link href="" className="btn btn-primary">
-                  Shop Now
+                </h1>
+                <h4
+                  className={`mt-4 ${style.shadow}`}
+                  style={{
+                    marginLeft: '75px',
+                  }}
+                >
+                  <strong>爸爸辛苦了！</strong>
+                </h4>
+                <Link
+                  href=""
+                  className="btn"
+                  style={{
+                    backgroundColor: '#e96d3f',
+                    color: 'white',
+                    marginLeft: '95px',
+                  }}
+                >
+                  開始購物
                 </Link>
               </div>
             </div>
-            <div className="product-offer rounded" style={{ height: '340px' }}>
+            <div className={`rounded ${style.bg2}`}>
               <Image
-                className="Image-fluid"
                 src="/cool.png"
                 alt=""
                 width={600}
                 height={335}
-                style={{
-                  opacity: '0.7',
-                  borderRadius: '5px',
-                }}
+                className={isHoveredSmallTwo ? style.hover : style.nohover}
+                onMouseEnter={() => handleMouseEnter('smallTwo')}
+                onMouseLeave={() => handleMouseLeave('smallTwo')}
               />
-              <div className="offer-text">
-                <h6 className="text-white text-uppercase">
+              <div className={style.content2}>
+                <h1
+                  className={style.shadow}
+                  style={{
+                    color: 'white',
+                  }}
+                >
                   <strong>\ 歡慶DEAL上線 /</strong>
-                </h6>
-                <h4 className="text-white mb-3">
-                  企業親子二手市集活動招募中！
-                </h4>
-                <a href="" className="btn btn-primary">
-                  Shop Now
-                </a>
+                </h1>
+                <h5
+                  className="mb-3 mt-3"
+                  style={{
+                    color: 'white',
+                    textShadow: 'rgba(0, 0, 0, 0.729)',
+                    marginLeft: '22px',
+                  }}
+                >
+                  <strong>企業親子二手市集活動招募中！</strong>
+                </h5>
+                <Link
+                  href=""
+                  className="btn"
+                  style={{
+                    backgroundColor: '#e96d3f',
+                    color: 'white',
+                    marginLeft: '120px',
+                  }}
+                >
+                  報名活動
+                </Link>
               </div>
             </div>
           </div>
