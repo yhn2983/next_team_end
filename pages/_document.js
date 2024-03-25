abc2982cf34add10403a579007267a69b182d10d
rayeraye1014 <rayeraye1014@gmail.com>
2024-03-26 01:42:51 +0800

    update

--- pages/_document.js
+++ pages/_document.js
@@ -11,10 +11,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-          defer
-        ></script>
       </body>
     </Html>
   )
