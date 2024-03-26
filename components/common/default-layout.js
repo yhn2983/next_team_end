111d246b4249c96f91c9b9dd69fbed4d20aaab2a
rayeraye1014 <rayeraye1014@gmail.com>
2024-03-26 11:18:17 +0800

    import category

--- components/common/default-layout.js
+++ components/common/default-layout.js
@@ -1,6 +1,7 @@
 import Navbar from './navbar'
 import Footer from './footer'
 import CarouselS1 from './carousel'
+import Category from './category'
 
 export default function DefaultLayout({ children, pageName = '' }) {
   return (
@@ -8,6 +9,7 @@ export default function DefaultLayout({ children, pageName = '' }) {
       <div className="container-fluid">
         <Navbar pageName={pageName} />
         <CarouselS1 pageName={pageName} />
+        <Category />
         <main className="container-fluid">{children}</main>
         <Footer pageName={pageName} />
       </div>
