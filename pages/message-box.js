079ff9054c38208dc9d4f6ab0b016d38bb4c74d0
Bochen <lee70468@gmail.com>
2024-04-02 13:31:57 +0800

    聊天室暫緩，可能要改方式製作

--- pages/message-box.js
+++ pages/message-box.js
@@ -17,12 +17,21 @@ export default function Profile() {
     <div className="col-lg-4">
       <div className={`card mb-4 ${styles.card}`}>
         <div className="card-body text-center">
-          <Image src="/imgs/ESG.png" alt="Profile" width={500} height={300} />{' '}
+          <Image src="/imgs/ESG.png" alt="Profile" width={50} height={30} />{' '}
           {/* 請確保這個路徑是正確的 */}
           <button onClick={() => setModalIsOpen(true)}>打開聊天訊息</button>
           <Modal
             isOpen={modalIsOpen}
             onRequestClose={() => setModalIsOpen(false)}
+            style={{
+              content: {
+                width: '700px', // 設定模態視窗內容的寬度
+                height: '700px', // 設定模態視窗內容的高度
+              },
+              overlay: {
+                backgroundColor: 'rgba(0, 0, 0, 0.5)', // 設定模態視窗背景的顏色
+              },
+            }}
           >
             <ChatMessage />
             <button onClick={() => setModalIsOpen(false)}>關閉聊天訊息</button>
