a4c0397942ef8033c0a3b51e9a4e2b4784878e2e
Bochen <lee70468@gmail.com>
2024-04-03 15:16:03 +0800

    補

--- pages/_document.js
+++ pages/_document.js
@@ -8,13 +8,10 @@ export default function Document() {
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
         rel="stylesheet"
       />
-<<<<<<< HEAD
-=======
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
       />
->>>>>>> lee
       <body>
         <Main />
         <NextScript />
