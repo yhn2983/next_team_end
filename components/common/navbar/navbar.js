0da5ed6aebdddf12d80b600519a96e4dadf079d2
rayeraye1014 <rayeraye1014@gmail.com>
2024-04-11 20:32:13 +0800

    fix the wrong time hover

--- components/common/navbar/navbar.js
+++ components/common/navbar/navbar.js
@@ -78,6 +78,7 @@ export default function CustomNavbar({ pageName = '' }) {
 
   // ---Hover status---
   const [isMessageHovered, setIsMessageHovered] = useState(false)
+  const [isUserHovered, setIsUserHovered] = useState(false)
   const [isLanHovered, setIsLanHovered] = useState(false)
   const [showLogin, setShowLogin] = useState(false)
   const [showRegister, setShowRegister] = useState(false)
@@ -85,10 +86,12 @@ export default function CustomNavbar({ pageName = '' }) {
 
   const handleMouseEnter = (type) => {
     if (type === 'message') setIsMessageHovered(true)
+    if (type === 'user') setIsUserHovered(true)
     if (type === 'language') setIsLanHovered(true)
   }
   const handleMouseLeave = (type) => {
     if (type === 'message') setIsMessageHovered(false)
+    if (type === 'user') setIsUserHovered(false)
     if (type === 'language') setIsLanHovered(false)
   }
 
@@ -207,7 +210,7 @@ export default function CustomNavbar({ pageName = '' }) {
                   onMouseEnter={() => handleMouseEnter('user')}
                   onMouseLeave={() => handleMouseLeave('user')}
                   style={{
-                    backgroundColor: auth.isAuth ? '#d6d4d4' : '#F5F5F5',
+                    backgroundColor: isUserHovered ? '#d6d4d4' : '#F5F5F5',
                     border: 'none',
                     color: '#8e2626',
                   }}
