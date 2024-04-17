2c51503502a47975e31f0ac566fe8b91ff2eab82
Bochen <lee70468@gmail.com>
2024-04-17 11:55:44 +0800

    更新faqs 把data裡面的資料弄進去

--- components/faq-accordion.js
+++ components/faq-accordion.js
@@ -1,35 +1,21 @@
 import React from 'react'
 import Accordion from 'react-bootstrap/Accordion'
+import data from '@/data/Faqs.json'
 
 export default function FaqAccordion() {
   return (
     <>
       <div className="container w-75">
         <Accordion defaultActiveKey="0" flush>
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="1">
-            <Accordion.Header>Accordion Item #2</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-          </Accordion.Item>
+          {data.map((item, index) => (
+            <Accordion.Item eventKey={index.toString()} key={index}>
+              <Accordion.Header>
+                {' '}
+                <strong>{item.header} </strong>
+              </Accordion.Header>
+              <Accordion.Body>{item.body}</Accordion.Body>
+            </Accordion.Item>
+          ))}
         </Accordion>
       </div>
     </>
